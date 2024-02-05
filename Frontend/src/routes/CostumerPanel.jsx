f7b63c8ab15fb3b6f7e63d6be0e943ab26c09bba
thisisehyas <maryamehyaaee@gmail.com>
2024-02-05 19:14:14 +0330

    Costumer Panel: created the order info component

--- Frontend/src/routes/CostumerPanel.jsx
+++ Frontend/src/routes/CostumerPanel.jsx
@@ -1,9 +1,13 @@
 import CostumerPanelHeader from "../components/Costumer/CostumerPanelHeader";
+import OrderInfoBox from "../components/Costumer/OrderInfoBox";
 
 const CostumerPanel = () => {
   return (
     <>
       <CostumerPanelHeader />
+      <div style={{ marginRight: "10px", marginLeft: "10px" }}>
+        <OrderInfoBox />
+      </div>
     </>
   );
 };
