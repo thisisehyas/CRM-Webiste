721b50d9b4fd79ff3a27f067b264e101acf7f77d
thisisehyas <maryamehyaaee@gmail.com>
2024-02-13 20:24:10 +0330

    Navbar: removed the enter button and added the user info icon

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,101 +1,5 @@
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import React from "react";
-// import Button from "react-bootstrap/Button";
-// import Container from "react-bootstrap/Container";
-// import Form from "react-bootstrap/Form";
-// import Nav from "react-bootstrap/Nav";
-// import Navbar from "react-bootstrap/Navbar";
-// import "../styles/navbar.css";
-// import { Link } from "react-router-dom/cjs/react-router-dom";
-// import LogoutButton from "./LogoutButton";
-
-// const Navbar1 = () => {
-//   return (
-//     <Navbar
-//       expand="lg"
-//       className="bg-body-tertiary fixed-top"
-//       style={{ textAlign: "right", direction: "rtl" }}
-//     >
-//       <Container fluid className="nav-container">
-//         <Navbar.Brand as={Link} to="/" className="navbar-brand">
-//           <img
-//             className="logo"
-//             src="../images/logo-navbar.png"
-//             alt="لوگوی شرکت"
-//             style={{ width: "61px", height: "83" }}
-//           />
-//         </Navbar.Brand>
-
-//         <Navbar.Toggle aria-controls="navbarScroll" />
-//         <Navbar.Collapse id="navbarScroll">
-//           <Nav
-//             className="navbar-nav me-5 my-2 my-lg-0"
-//             style={{ maxHeight: "100px" }}
-//             navbarScroll
-//           >
-//             <Nav.Link as={Link} to="/Products" className="nav-link ms-3">
-//               محصولات
-//             </Nav.Link>
-//             <Nav.Link as={Link} to="/Services" className="nav-link ms-3">
-//               خدمات
-//             </Nav.Link>
-//             <Nav.Link as={Link} to="/SampleWorks" className="nav-link ms-3">
-//               نمونه کارها
-//             </Nav.Link>
-//             <Nav.Link as={Link} to="/About" className="nav-link ms-3">
-//               درباره ما
-//             </Nav.Link>
-//             <Nav.Link as={Link} to="/Contact" className="nav-link ms-3">
-//               تماس با ما
-//             </Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-
-//         <div>
-//           <Button
-//             as={Link}
-//             to="/login"
-//             variant="success"
-//             style={{
-//               color: "white",
-//               backgroundColor: "#56B94C",
-//               borderColor: "#56B94C",
-//             }}
-//             className="enter"
-//           >
-//             ورود
-//           </Button>
-//           <LogoutButton />
-//         </div>
-//       </Container>
-//     </Navbar>
-//   );
-// };
-
-// export default Navbar1;
-
-// // Things to fix:
-
-// // - the logo should be a button that goes to the home page.
-// // - you can adjust the positions for when the navbar is opened
-// //    the carousel won't go under it so it is still visible
-// // - Complete the href of the navbar links.
-// // - The hover effect of the
-// //    - each navlink
-// // - The magnifier needs to be a button for search
-// // - The logo should be in the middle after the search bar and before the
-// //    toggler menu.
-// // - When the menu is opened for scroll the navbar should take the whole
-// //    width of the horizontal line. But when the menu is closed it should
-// //    go back. Do this with js.
-// //    If it only sticked to the right of the page without sapce, that
-// //    would be ok too.
-// //  - The search bar, when you try to right in it, the direction is not
-// //    from right to left. make it happen to be better for persion typing.
-// //  - Generally speaking I don't like the responsiveness of the navbar. it is responsive but it's ugly. so fix it.
-
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -104,8 +8,44 @@ import Navbar from "react-bootstrap/Navbar";
 import "../styles/navbar.css";
 import { Link } from "react-router-dom/cjs/react-router-dom";
 import LogoutButton from "./LogoutButton";
+import { Modal } from "react-bootstrap";
+import axios from "axios";
+import { getAccessToken } from "./authUtils";
 
 const Navbar1 = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [userData, setUserData] = useState(null);
+
+  // Function to handle hovering over the user icon
+  const handleUserIconClick = () => {
+    setShowModal(true);
+  };
+
+  // Function to close the modal
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(
+          "http://127.0.0.1:8000/auth/users/me/",
+          {
+            headers: {
+              Authorization: `JWT ${getAccessToken()}`,
+            },
+          }
+        );
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
   return (
     <Navbar
       expand="lg"
@@ -126,7 +66,7 @@ const Navbar1 = () => {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="navbar-nav ms-auto mb-2 mb-lg-0"
-            style={{ maxHeight: "100px", marginRight:'2%' }}
+            style={{ maxHeight: "100px", marginRight: "2%" }}
             navbarScroll
           >
             <Nav.Link as={Link} to="/Products" className="nav-link ms-3">
@@ -147,24 +87,57 @@ const Navbar1 = () => {
           </Nav>
 
           <div className="d-flex">
-            <Button
-              as={Link}
-              to="/login"
-              variant="success"
-              style={{
-                color: "white",
-                backgroundColor: "#56B94C",
-                borderColor: "#56B94C",
-                marginRight: "10%", 
-              }}
-              className="enter me-2"
+            <Nav.Item
+              style={{ cursor: "pointer" }}
+              className="mx-2"
+              onClick={handleUserIconClick}
             >
-              ورود
-            </Button>
+              <svg
+                className="mb-2"
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-user"
+              >
+                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+            </Nav.Item>
             <LogoutButton />
           </div>
         </Navbar.Collapse>
       </Container>
+      {/* User info modal */}
+      <Modal
+        style={{ direction: "rtl", textAlign: "right" }}
+        show={showModal}
+        onHide={handleCloseModal}
+      >
+        <Modal.Header>
+          <Modal.Title>اطلاعات کاربر</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {userData && (
+            <>
+              <p>نام: {userData.first_name}</p>
+              <p>نام خانوادگی: {userData.last_name}</p>
+              <p>ایمیل: {userData.email}</p>
+              <p>نام کاربری: {userData.username}</p>
+            </>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            بستن
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Navbar>
   );
 };
