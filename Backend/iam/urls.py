2db129441a374f73323a054f2b785614c42b5163
thisisehyas <maryamehyaaee@gmail.com>
2024-03-15 00:50:17 +0330

    The new backend (not mine)

--- Backend/iam/urls.py
+++ Backend/iam/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from .views import signup, login, token_refresh, verify_code
+from .views import signup, login, token_refresh, verify_code, resend_verification_code
 
 # Swagger Schema
 schema_view = get_schema_view(
@@ -22,6 +22,7 @@ urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
     path('token-refresh/', token_refresh, name='token_refresh'),
+    path('resend-verification-code/', resend_verification_code, name='resend_verification_code'),
     path('verify-code/', verify_code, name='verify_code'),
     # Swagger URL
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
--- Backend/iam/views.py
+++ Backend/iam/views.py
@@ -6,9 +6,13 @@ from drf_yasg.utils import swagger_auto_schema
 from .serializers import UserSerializer
 from .models import CustomUser
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import HttpRequest
 import redis
 import random
 import string
+import time
+from django.utils import timezone
+
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 verification_code_ttl = 60
@@ -17,6 +21,7 @@ def generate_verification_code():
     code = ''.join(random.choices(string.digits, k=6))
     return code
 
+temp_user_data = {}
 
 @swagger_auto_schema(
     method='post', 
@@ -27,8 +32,9 @@ def generate_verification_code():
             'last_name': openapi.Schema(type=openapi.TYPE_STRING),
             'phone_number': openapi.Schema(type=openapi.TYPE_STRING),
             'password': openapi.Schema(type=openapi.TYPE_STRING),
+            'code': openapi.Schema(type=openapi.TYPE_STRING),  
         },
-        required=['first_name', 'last_name', 'phone_number', 'password']
+        required=['first_name', 'last_name', 'phone_number', 'password', 'code']  
     ),
     responses={
         status.HTTP_201_CREATED: openapi.Response(
@@ -42,48 +48,31 @@ def generate_verification_code():
     operation_description="Register a new user with the provided information. After successful registration, an access token and a refresh token will be returned."
 )
 @api_view(['POST'])
-
 def signup(request):
-    """
-    Register a new user.
-    """
     data = request.data
     serializer = UserSerializer(data=data)
     if serializer.is_valid():
-        first_name = serializer.validated_data.get('first_name')
-        last_name = serializer.validated_data.get('last_name')
-        phone_number = serializer.validated_data.get('phone_number')
-        password = serializer.validated_data.get('password')
-
+        phone_number = data['phone_number']
+        
+        existing_user = CustomUser.objects.filter(phone_number=phone_number).first()
+        if existing_user:
+            redis_client.delete(phone_number)
+        
         user = CustomUser.objects.create_user(
-            username=phone_number, 
-            first_name=first_name,
-            last_name=last_name,
+            username=phone_number,
+            first_name=data['first_name'],
+            last_name=data['last_name'],
             phone_number=phone_number,
-            password=password,  # Set the password
-            is_active=True
+            password=data['password'],
+            is_active=False
         )
-
-        response_data = {'message': 'User created successfully'}
-
-        refresh = RefreshToken.for_user(user)
-        access_token = refresh.access_token
-        response_data['access_token'] = str(access_token)
-        response_data['refresh_token'] = str(refresh)
-
-        # Send verification code
         verification_code = generate_verification_code()
         redis_client.setex(phone_number, verification_code_ttl, verification_code)
         print(f'Verification code for {phone_number}: {verification_code}')
 
-        # You can also call verify_code here to automatically verify the code after signup
-        # verify_code(request)
-
+        response_data = {'message': 'User created successfully'}
         return Response(response_data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
 @swagger_auto_schema(
     method='post',
     request_body=openapi.Schema(
@@ -174,6 +163,10 @@ def token_refresh(request):
         return Response({'message': 'Invalid refresh token'}, status=400)
 
 
+from django.utils import timezone
+
+
+
 @swagger_auto_schema(
     method='post',
     request_body=openapi.Schema(
@@ -185,21 +178,89 @@ def token_refresh(request):
         required=['phone_number', 'code']
     ),
     responses={
-        status.HTTP_200_OK: openapi.Response(description='Verification code is valid'),
-        status.HTTP_400_BAD_REQUEST: openapi.Response(description='Invalid verification code')
+        status.HTTP_200_OK: openapi.Response(description='Access and refresh tokens generated successfully'),
+        status.HTTP_400_BAD_REQUEST: openapi.Response(description='Invalid verification code or user not found')
     },
-    operation_summary="Verify verification code",
+    operation_summary="Verify verification code and generate tokens",
     tags=["Authentication"],
-    operation_description="Verify the provided verification code for a given phone number."
+    operation_description="Verify the provided verification code for a given phone number and generate access and refresh tokens for the user."
 )
 @api_view(['POST'])
 def verify_code(request):
+    """
+    Verify the provided verification code for a given phone number and generate tokens.
+    """
     data = request.data
     phone_number = data.get('phone_number')
     code = data.get('code')
 
-    saved_code = redis_client.get(phone_number)
-    if saved_code and saved_code.decode('utf-8') == code:
-        return Response({'message': 'Verification code is valid'})
+    saved_data = redis_client.get(phone_number)
+    if saved_data:
+        saved_code = saved_data.decode('utf-8')
+        if saved_code == code:
+            redis_client.delete(phone_number)
+
+            try:
+                user = CustomUser.objects.get(phone_number=phone_number)
+            except CustomUser.DoesNotExist:
+                user = CustomUser.objects.create_user(
+                    username=phone_number,
+                    phone_number=phone_number,
+                    is_active=True
+                )
+
+            refresh = RefreshToken.for_user(user)
+            access_token = refresh.access_token
+
+            return Response({
+                'access_token': str(access_token),
+                'refresh_token': str(refresh)
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid verification code'}, status=status.HTTP_400_BAD_REQUEST)
     else:
-        return Response({'message': 'Invalid verification code'}, status=400)
+        return Response({'message': 'Verification code expired or not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+from django.utils import timezone
+@swagger_auto_schema(
+    method='post',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'phone_number': openapi.Schema(type=openapi.TYPE_STRING),
+        },
+        required=['phone_number']
+    ),
+    responses={
+        status.HTTP_200_OK: openapi.Response(description='Verification code resent successfully'),
+        status.HTTP_400_BAD_REQUEST: openapi.Response(description='Invalid input')
+    },
+    operation_summary="Resend verification code",
+    tags=["Authentication"],
+    operation_description="Resend the verification code to the provided phone number."
+)
+
+@api_view(['POST'])
+def resend_verification_code(request):
+    phone_number = request.data.get('phone_number')
+    
+    user = CustomUser.objects.filter(phone_number=phone_number).first()
+    if user:
+        if user.is_active:
+            return Response({'message': 'User is already active'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        last_code_sent_time = redis_client.get(f'{phone_number}_last_code_sent_time')
+        if last_code_sent_time:
+            last_sent_time = timezone.make_aware(datetime.fromisoformat(last_code_sent_time.decode('utf-8')))
+            elapsed_time = timezone.now() - last_sent_time
+            if elapsed_time.seconds < 60:
+                return Response({'message': 'Please wait before requesting a new verification code'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        verification_code = generate_verification_code()
+        redis_client.setex(phone_number, verification_code_ttl, verification_code)
+        print(f'New verification code for {phone_number}: {verification_code}')
+        redis_client.set(f'{phone_number}_last_code_sent_time', timezone.now().isoformat())
+        
+        return Response({'message': 'Verification code sent successfully'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': 'User does not exist'}, status=status.HTTP_404_NOT_FOUND)
