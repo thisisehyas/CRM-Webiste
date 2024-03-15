25c0f772d3fe303e7bfc8d186e4f94aa6920b705
thisisehyas <maryamehyaaee@gmail.com>
2024-03-15 11:41:21 +0330

    Used routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Login from "./routes/Login.jsx";
 import SignUp from "./routes/Signup.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <SignUp />
-      <Login />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div>Hello</div>
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
