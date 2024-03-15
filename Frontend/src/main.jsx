1efaa14f1f8455d91834b26f8d6a26a355d36aba
thisisehyas <maryamehyaaee@gmail.com>
2024-03-15 11:39:20 +0330

    used ReactDOM.render instead of ReactDOM.createRoot().render to solve routing issue

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
